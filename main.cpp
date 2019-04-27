dacd4e448e4d30c50568c5f29ec9dfa064f4014d
JFergen <josephfergen150@gmail.com>
2019-04-27 10:59:04 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int emptyBottles;
+    
+    cout << "Enter number of empty soda bottles: ";
+    cin >> emptyBottles;
+    return 0;
+}
